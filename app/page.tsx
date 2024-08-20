2378d0d79a67d8c82539166dc02240dcf1bcbd60
yui-119 <poppop0206@icloud.com>
2024-08-20 14:27:14 +0900

    タイトルの色変更

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+    <div className={styles.decolationh1} role="presentation">
+      テニスゲーム
+    </div>
      <h1 className={styles.h1}>
       テニスゲーム
     </h1>
