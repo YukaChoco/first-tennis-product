1d7fa8a36c7abb7a16611608765d18ba5122cf3c
yui-119 <poppop0206@icloud.com>
2024-08-20 15:15:18 +0900

    clickをconsoleに出力

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        console.log("Enterされました");
+      }
+      if (event.key === "p") {
+        console.log("pがおされました");
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <main className={styles.main}>
     <div className={styles.decolationh1} role="presentation">
       テニスゲーム
     </div>
-     <h1 className={styles.h1}>
+     <h1 className={styles.h1} onClick={()=>console.log('clickされました')}>
       テニスゲーム
     </h1>
      <p className={styles.p}>
@@ -19,6 +35,7 @@ export default function Home() {
      　width={200}
        height={150}
      />
+     <a href="/game-instructions">link</a>
     </main>
   );
 }
