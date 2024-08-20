2513158d0fbcc3d6650099966eb040ffc1009ac5
yui-119 <poppop0206@icloud.com>
2024-08-20 16:10:46 +0900

    的表示

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -4,12 +4,16 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Image className= {styles.hiyoko}
+      <Image className={styles.hiyoko}
      　src="打つ前のひよこ.svg" 
      　alt="打つ前のひよこ画像" 
      　width={200}
        height={150}
      />
+     <div className={styles.container}>
+     <div className={styles.target}></div>
+     <div className={styles.target2}></div>
+     </div>
     </main>
       );
 }
