2a48211185b97ddde21db65199d40197462d9c7b
yui-119 <poppop0206@icloud.com>
2024-08-20 16:15:45 +0900

    ひよこの場所指定

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -4,14 +4,13 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Image className={styles.hiyoko}
+     <div className={styles.container}>
+     <div className={styles.target}><Image className={styles.hiyoko}
      　src="打つ前のひよこ.svg" 
      　alt="打つ前のひよこ画像" 
      　width={200}
        height={150}
-     />
-     <div className={styles.container}>
-     <div className={styles.target}></div>
+     /></div>
      <div className={styles.target2}></div>
      </div>
     </main>
